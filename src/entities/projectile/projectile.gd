9368c2efd35a4f6ea735cf53d76da9e4ede81d5d
Zircon <zircon332@gmail.com>
2022-12-24 00:13:09 +0800

    initialized tests

--- src/entities/projectile/projectile.gd
+++ src/entities/projectile/projectile.gd
@@ -1,11 +1,16 @@
 extends Area2D
 
+
 export(int) var speed := 500
 
-func _ready():
+
+func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta) -> void:
+	move()
+
+
+func move() -> void:
+	pass
