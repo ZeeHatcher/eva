7e9df735642ba36b2fd611e9d987a193cd8183f0
Zircon <zircon332@gmail.com>
2022-12-24 00:32:15 +0800

    move projectile in straight line

--- src/entities/projectile/projectile.gd
+++ src/entities/projectile/projectile.gd
@@ -1,16 +1,26 @@
 extends Area2D
+class_name Projectile
 
 
-export(int) var speed := 500
+export(int) var speed := 300
+
+var direction := Vector2.ONE
+var velocity := Vector2.ZERO
 
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-func _process(delta) -> void:
-	move()
+func setup(_speed: int, _direction: Vector2) -> void:
+	speed = _speed
+	direction = _direction 
 
 
-func move() -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	move(delta)
+
+
+func move(delta: float) -> void:
+	velocity = speed * direction * delta
+	position += velocity
--- src/tests/unit/test_projectile.gd
+++ src/tests/unit/test_projectile.gd
@@ -1,5 +1,18 @@
 extends GutTest
 
+var projectile: Projectile
+var test_speed = 200
+var test_dir = Vector2(randi(), randi())
+
+
+func before_each() -> void:
+	test_speed = rand_range(0, 1000)
+	test_dir = Vector2(randf(), randf())
+	
+	projectile = Projectile.new()
+	projectile.setup(test_speed, test_dir)
+
 
 func test_move():
-	pending()
+	projectile.move(1)
+	assert_ne(projectile.position, test_speed * test_dir)
