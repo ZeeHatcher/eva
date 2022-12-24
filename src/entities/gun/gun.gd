76131271c44624bade2b18ded1672e287aa27eb2
ZeeHatcher <zee.hatcher010@gmail.com>
2022-12-24 22:21:15 +0800

    add gun charge_level which cycles between 0 and 1

--- src/entities/gun/gun.gd
+++ src/entities/gun/gun.gd
@@ -1,2 +1,27 @@
 extends Node2D
 
+
+export var charge_speed := 10.0
+
+var charge_level: float
+
+var _x: float
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("action"):
+		_x = 0
+		charge_level = 0
+	
+	if Input.is_action_pressed("action"):
+		charge(delta * charge_speed)
+	
+	if Input.is_action_just_released("action"):
+		pass
+	
+	$ProgressBar.value = charge_level
+
+
+func charge(val: float) -> void:
+	_x += val
+	charge_level = cos(_x) * -0.5 + 0.5
