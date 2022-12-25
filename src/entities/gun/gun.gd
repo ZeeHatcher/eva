0621e34be8a244bd6ba6ab9945a799796054de98
ZeeHatcher <zee.hatcher010@gmail.com>
2022-12-25 16:53:53 +0800

    vary spread, damage, projectile count, speed based on charge_level

--- src/entities/gun/gun.gd
+++ src/entities/gun/gun.gd
@@ -16,30 +16,55 @@ onready var _barrel := $Barrel
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("action"):
-		_x += delta * charge_speed
-		charge_level = _charge(_x)
+		_charge(delta * charge_speed)
 	
 	$ProgressBar.value = charge_level
 
 
 func _unhandled_input(_event: InputEvent):
 	if Input.is_action_just_pressed("action"):
-		_x = 0
-		charge_level = 0
+		_reset_charge()
 	
 	if Input.is_action_just_released("action"):
-		var projectile := ProjectileScene.instance()
-		projectile.setup(
-				charge_level * 100 + 10,
-				Vector2.LEFT.rotated(global_rotation),
-				charge_level * 900 + 100,
-				_barrel.global_position)
-		context.add_child(projectile)
+		_shoot()
 
 
 func setup(ctx: Node) -> void:
 	context = ctx
 
 
-func _charge(val: float) -> float:
-	return cos(val) * -0.5 + 0.5
+func _charge(val: float) -> void:
+	_x += val
+	charge_level = cos(_x) * -0.5 + 0.5
+
+
+func _reset_charge() -> void:
+	_x = 0
+	charge_level = 0
+
+
+func _shoot() -> void:
+	var projectile_count := int(charge_level * -10) + 10
+	var damage := charge_level * 100 + 10
+	var speed := charge_level * 900 + 100
+	var spread := range_lerp(charge_level, 1.0, 0.0, 0, deg2rad(60))
+	
+	for n in range(projectile_count):
+		var projectile := ProjectileScene.instance()
+		var direction := global_rotation
+		
+		projectile.setup(
+				damage,
+				_calculate_direction(projectile_count, n, spread),
+				speed,
+				_barrel.global_position)
+		context.add_child(projectile)
+
+
+func _calculate_direction(count: int, index: int, spread: float) -> Vector2:
+	var offset = 0.0
+	
+	if count > 1:
+		offset = range_lerp(index, 0, count - 1, -spread, spread)
+	
+	return Vector2.LEFT.rotated(global_rotation + offset)
