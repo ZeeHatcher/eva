54fff26e04af73a4f5b1514b4221175061affdfc
ZeeHatcher <zee.hatcher010@gmail.com>
2022-12-28 00:12:01 +0800

    scale spawns based on time

--- src/etc/stopwatch/stopwatch.gd
+++ src/etc/stopwatch/stopwatch.gd
@@ -2,9 +2,12 @@ extends Node
 class_name Stopwatch
 
 
+signal tick
+
 export(float) var time_elapsed setget set_time_elapsed, get_time_elapsed
 
 var _paused := true
+var _prev: int
 
 
 func _process(delta: float) -> void:
@@ -12,6 +15,12 @@ func _process(delta: float) -> void:
 		return
 	
 	time_elapsed += delta
+	
+	var current := seconds()
+	if current > _prev:
+		emit_signal("tick")
+	
+	_prev = current
 
 
 func seconds() -> int:
