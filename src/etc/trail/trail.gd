a41ca462958937983e33e9e4f252d70356fb1dc4
Zircon <zircon332@gmail.com>
2023-01-02 10:52:03 +0800

    fixed trail not queued free after no more points

--- src/etc/trail/trail.gd
+++ src/etc/trail/trail.gd
@@ -10,9 +10,16 @@ func add_trail(pos) -> void:
 
 
 func _process(delta) -> void:
+	# delete trail if used up
+	if not has_source and get_point_count() <= 0:
+		queue_free()
+		return
+	
+	# remove a point every frame
 	if not has_source:
 		remove_point(0)
 		return
 	
+	# remove all excess points
 	while get_point_count() > trail_length:
 		remove_point(0)
