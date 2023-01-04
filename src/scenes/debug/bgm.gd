a468d4f300c1118266ab810775ce23f71707021e
Zircon <zircon332@gmail.com>
2023-01-05 00:21:55 +0800

    fixed game end music loop bug

--- src/scenes/debug/bgm.gd
+++ src/scenes/debug/bgm.gd
@@ -1,7 +1,7 @@
 extends AudioStreamPlayer
 
 var states = {"Start": 0, "Main": 1, "End": 2}
-var loops = {0: [0, 16], 1: [16, 80], 2: [80, -1]}
+var loops = {0: [0, 16], 1: [16, 80], 2: [80, 90]}
 
 var current_state = 0
 
@@ -10,16 +10,21 @@ func _ready() -> void:
 	play(0)
 
 
-func set_loop(_state) -> void:
-	current_state = states[_state]
-	
-	if current_state == 2:
-		play(loops[current_state][0])
-
-
 func _process(delta) -> void:
-	if loops[current_state][1] == -1:
+	if is_last_loop() and get_playback_position() >= loops[current_state][1]:
+		stop()
 		return
 	
 	if get_playback_position() >= loops[current_state][1]:
 		play(loops[current_state][0])
+
+
+func set_loop(_state) -> void:
+	current_state = states[_state]
+	
+	if is_last_loop():
+		play(loops[current_state][0])
+
+
+func is_last_loop() -> bool:
+	return current_state == (loops.size() - 1)
